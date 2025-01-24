ca20dd422c48e0b68a1e2c02310145ae850131c2
seveibar <seveibar@gmail.com>
2025-01-23 21:05:59 -0800

    adjust useColorMode hook to refresh every component

--- src/hooks/use-color-mode.ts
+++ src/hooks/use-color-mode.ts
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react"
 import { useColorMode as useColorModeOg } from "@docusaurus/theme-common"
 
-globalThis.___USE_COLOR_MODE_HOOK_HAS_TRIGGERED_UPDATE = false
-
 /**
  * This hook ensures that isDarkTheme has received a post-page-load update
  * value. Unfortunately there is a bug where isDarkTheme is not accurate
@@ -14,13 +12,8 @@ export const useColorMode = () => {
   const [notMatchedAfterReload, setNotMatchedAfterReload] = useState(false)
 
   useEffect(() => {
-    if (globalThis.___USE_COLOR_MODE_HOOK_HAS_TRIGGERED_UPDATE) {
-      return
-    }
-
     setTimeout(() => {
       setNotMatchedAfterReload(true)
-      globalThis.___USE_COLOR_MODE_HOOK_HAS_TRIGGERED_UPDATE = true
     }, 100)
   }, [])
 
