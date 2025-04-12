6e5690c3fac8a117db66a7005bfce644a6593e54
Arnav K <arnavkaushal09@gmail.com>
2025-04-12 14:31:08 +0530

    temp add prod url, might break dev but when pushed to prod will work

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,13 +64,13 @@ const config: Config = {
       { name: "twitter:card", content: "summary" }, // Small preview with logo
       // { name: "twitter:title", content: "tscircuit Docs" },
       // { name: "twitter:description", content: "Create Electronics with TypeScript and React" },
-      { name: "twitter:image", content: "/logo/logo.png" },
+      { name: "twitter:image", content: "https://docs.tscircuit.com/logo/logo.png" },
 
       // LinkedIn & Open Graph Meta Tags
       { property: "og:type", content: "website" },
       { property: "og:title", content: "Tscircuit Docs" },
       { property: "og:description", content: "Create Electronics with TypeScript and React" },
-      { property: "og:image", content: "/logo/logo.png" },
+      { property: "og:image", content: "https://docs.tscircuit.com/logo/logo.png" },
       { property: "og:url", content: "https://docs.tscircuit.com/" },
     ],
     navbar: {
