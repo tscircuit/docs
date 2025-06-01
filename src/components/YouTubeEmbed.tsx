ea60de4f1cc6263a34b27af13b66e46c672d719d
Ansh Grover <168731971+Anshgrover23@users.noreply.github.com>
2025-06-01 21:08:53 +0530

    change youtube video link (#64)

---
+++ src/components/YouTubeEmbed.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+interface YouTubeEmbedProps {
+  youtubeId: string;
+}
+
+const YouTubeEmbed: React.FC<YouTubeEmbedProps> = ({ youtubeId }) => {
+  return (
+    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%' }}>
+      <iframe
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+        src={`https://www.youtube.com/embed/${youtubeId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      >
+      </iframe>
+    </div>
+  )
+}
+
+export default YouTubeEmbed
