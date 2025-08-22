75ed8160d7356de46a2544bbdbaaf9f44985d8c5
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-08-23 03:11:35 +0530

    change the cdn url from jsdeliver to cloudflare (#113)
    * change the cdn url from jsdeliver to cloudflare
    
    * Update running-tscircuit-standalone.mdx
    
    * use react component to fetch the latest version
    
    * use default export
    
    * remove process env

---
+++ src/components/TscircuitCdnCodeBlockExample.tsx
@@ -0,0 +1,28 @@
+import CodeBlock from "@theme/CodeBlock"
+import { useLatestTscircuitCdnUrl } from "../hooks/use-latest-tscircuit-cdn-url"
+
+export default () => {
+  const url = useLatestTscircuitCdnUrl()
+
+  if (!url) return null
+
+  return (
+    <CodeBlock language="html">{`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Pull the latest browser bundle from cdnjs -->
+    <script type="module" src="${url}"></script>
+
+    <!-- Your tscircuit snippet goes here -->
+    <script type="tscircuit-tsx">
+      export default () => (
+        <board width="10mm" height="10mm">
+          <resistor name="R1" resistance={1000} footprint={"0402"} />
+        </board>
+      )
+    </script>
+  </head>
+  <body></body>
+</html>`}</CodeBlock>
+  )
+}
