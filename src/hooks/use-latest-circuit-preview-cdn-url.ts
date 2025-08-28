2171ec9d9ef2d41d19b666c8e812af9208c0a1ec
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-08-29 04:09:35 +0530

    Add the guide `How to use tscircuit with chatgpt` (#115)
    * Add the guide `How to use tscircuit with chatgpt`
    
    * format

---
+++ src/hooks/use-latest-circuit-preview-cdn-url.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+export const useLatestCircuitPreviewCdnUrl = () => {
+  const [url, setUrl] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetch("https://registry.npmjs.org/@tscircuit/circuit-preview/latest")
+      .then((res) => res.json())
+      .then((data: { version: string }) => {
+        setUrl(
+          `https://unpkg.com/@tscircuit/circuit-preview@${data.version}/dist/index.global.js`,
+        )
+      })
+      .catch(() => {})
+  }, [])
+
+  return url
+}
