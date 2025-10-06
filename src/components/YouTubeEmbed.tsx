e4e748fc03587502bc5f6a5fc970094586f116ae
Severin Ibarluzea <seveibar@gmail.com>
2025-10-06 15:34:46 -0700

    style: wrap documentation media in rounded containers (#176)
    * style: add rounded media containers
    
    * fix: prevent markdown media stretching
    
    * fix: frame chatgpt guide images

--- src/components/YouTubeEmbed.tsx
+++ src/components/YouTubeEmbed.tsx
@@ -6,30 +6,18 @@ interface YouTubeEmbedProps {
 
 const YouTubeEmbed: React.FC<YouTubeEmbedProps> = ({ youtubeId }) => {
   return (
-    <div
-      style={{
-        position: "relative",
-        paddingBottom: "56.25%",
-        height: 0,
-        overflow: "hidden",
-        maxWidth: "100%",
-      }}
-    >
-      <iframe
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-        }}
-        src={`https://www.youtube.com/embed/${youtubeId}`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      />
+    <div className="youtube-embed__container">
+      <div className="youtube-embed__frame">
+        <iframe
+          className="youtube-embed__iframe"
+          src={`https://www.youtube.com/embed/${youtubeId}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      </div>
     </div>
   )
 }
