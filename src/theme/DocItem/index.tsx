14856cf4f26a84bafb4d8a02e512a6518e7189d9
bitsbyritik <bitsbyritik@gmail.com>
2025-10-08 13:41:13 +0530

    fix(UI): enanhce ui

---
+++ src/theme/DocItem/index.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import DocItem from "@theme-original/DocItem"
+import type DocItemType from "@theme/DocItem"
+import type { WrapperProps } from "@docusaurus/types"
+import { useCurrentSidebarCategory } from "@docusaurus/theme-common/internal"
+type Props = WrapperProps<typeof DocItemType>
+
+export default function DocItemWrapper(props: Props): React.JSX.Element {
+  const currentCategory = useCurrentSidebarCategory() as
+    | { label: string }
+    | undefined
+  return (
+    <>
+      {currentCategory && (
+        <div className="doc-subtitle">{currentCategory.label}</div>
+      )}
+      <DocItem {...props} />
+    </>
+  )
+}
