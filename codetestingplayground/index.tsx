79494c32c402c98ca9407586632840eff4b02ae6
seveibar <seveibar@gmail.com>
2026-01-15 08:37:06 -0800

    add code testing playground

---
+++ codetestingplayground/index.tsx
@@ -0,0 +1,7 @@
+export default () => (
+  <board>
+    <resistor resistance="1k" footprint="0402" name="R1" schX={3} pcbX={3} />
+    <capacitor capacitance="1000pF" footprint="0402" name="C1" schX={-3} pcbX={-3} />
+    <trace from=".R1 > .pin1" to=".C1 > .pin1" />
+  </board>
+)
