cffca24afcad1354626a6a02558dd59b059db985
suhail-ak-2 <35195014+suhail-ak-2@users.noreply.github.com>
2026-05-11 17:29:18 +0530

    Fix docs content centering

--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -1,13 +1,11 @@
 import React, { type ReactNode } from "react"
 import clsx from "clsx"
-import { useWindowSize } from "@docusaurus/theme-common"
 import { useDoc } from "@docusaurus/plugin-content-docs/client"
 import DocItemPaginator from "@theme/DocItem/Paginator"
 import DocVersionBanner from "@theme/DocVersionBanner"
 import DocVersionBadge from "@theme/DocVersionBadge"
 import DocItemFooter from "@theme/DocItem/Footer"
 import DocItemTOCMobile from "@theme/DocItem/TOC/Mobile"
-import DocItemTOCDesktop from "@theme/DocItem/TOC/Desktop"
 import DocItemContent from "@theme/DocItem/Content"
 import DocBreadcrumbs from "@theme/DocBreadcrumbs"
 import ContentVisibility from "@theme/ContentVisibility"
@@ -21,22 +19,15 @@ import styles from "./styles.module.css"
  */
 function useDocTOC() {
   const { frontMatter, toc } = useDoc()
-  const windowSize = useWindowSize()
 
   const hidden = frontMatter.hide_table_of_contents
   const canRender = !hidden && toc.length > 0
 
   const mobile = canRender ? <DocItemTOCMobile /> : undefined
 
-  const desktop =
-    canRender && (windowSize === "desktop" || windowSize === "ssr") ? (
-      <DocItemTOCDesktop />
-    ) : undefined
-
   return {
     hidden,
     mobile,
-    desktop,
   }
 }
 
@@ -44,8 +35,8 @@ export default function DocItemLayout({ children }: Props): ReactNode {
   const docTOC = useDocTOC()
   const { metadata } = useDoc()
   return (
-    <div className="row">
-      <div className={clsx("col", !docTOC.hidden && styles.docItemCol)}>
+    <div className={clsx("row", styles.docItemRow)}>
+      <div className={clsx("col", styles.docItemCol)}>
         <ContentVisibility metadata={metadata} />
         <DocVersionBanner />
         <div className={styles.docItemContainer}>
@@ -66,7 +57,6 @@ export default function DocItemLayout({ children }: Props): ReactNode {
           <DocItemPaginator />
         </div>
       </div>
-      {docTOC.desktop && <div className="col col--3">{docTOC.desktop}</div>}
     </div>
   )
 }
