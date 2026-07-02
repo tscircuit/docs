485a0906a95fbe6107e1cf1df6d8d0ffd0d128df
imrishabh18 <belikerishabh@gmail.com>
2026-07-03 00:40:43 +0530

    switch to icon

--- src/components/CircuitPreview/CircuitPreview.tsx
+++ src/components/CircuitPreview/CircuitPreview.tsx
@@ -121,7 +121,23 @@ const TryInEditorLink = ({ href }: { href: string }) => (
     aria-label="Open this circuit in the tscircuit editor"
     title="Open this circuit in the tscircuit editor"
   >
-    Try in editor
+    <svg
+      aria-hidden="true"
+      className="circuit-preview-open-editor-button-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1.125rem"
+      height="1.125rem"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M15 3h6v6" />
+      <path d="M10 14 21 3" />
+      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+    </svg>
   </a>
 )
 
